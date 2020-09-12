6d6384ce621dabfe8616c893a460db7677c4b69e
budougumi0617 <budougumi0617@binc.jp>
2020-09-12 23:22:45 +0900

    :sparkles: generate test file

--- generate.go
+++ generate.go
@@ -1,8 +1,13 @@
 package leetgode
 
 import (
+	"bytes"
 	"context"
 	"fmt"
+	"io/ioutil"
+	"text/template"
+
+	"github.com/cweill/gotests"
 )
 
 type Format struct {
@@ -17,7 +22,15 @@ type Format struct {
 }
 
 const submitFile = `
+package main
 
+/**
+ * {{.Content}}
+**/
+/**
+ * {{.SampleTestCase}}
+**/
+{{.CodeDefinition.DefaultCode}}
 `
 
 // TODO: Goの関数定義を見つける
@@ -42,5 +55,33 @@ func GenerateCmd(ctx context.Context, id string) error {
 		return fmt.Errorf("not found the code for Go")
 	}
 	fmt.Printf("%s\n", fmt.Sprint(c.DefaultCode))
+	input := &Format{
+		Referer:        q.Referer,
+		Content:        q.Content,
+		CodeDefinition: c,
+		SampleTestCase: q.SampleTestCase,
+	}
+	tmpl, err := template.New("submitFile").Parse(submitFile)
+	if err != nil {
+		panic(err)
+	}
+	var buf bytes.Buffer
+	err = tmpl.Execute(&buf, input)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("%s", buf.String())
+	path := "tmp/hoge.go"
+	if err := ioutil.WriteFile(path, buf.Bytes(), 0644); err != nil {
+		return err
+	}
+	tess, err := gotests.GenerateTests(path, nil)
+	if err != nil {
+		return err
+	}
+	fmt.Printf("%q\n", tess[0].Path)
+	if err := ioutil.WriteFile(tess[0].Path, tess[0].Output, 0644); err != nil {
+		return err
+	}
 	return nil
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/budougumi/leetgode
 
 go 1.15
 
-require github.com/google/go-cmp v0.5.2
+require (
+	github.com/cweill/gotests v1.5.3
+	github.com/google/go-cmp v0.5.2
+	golang.org/x/tools v0.0.0-20200911193555-6422fca01df9 // indirect
+)
