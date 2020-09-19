a61d8a0594daf3e9d9f6e72d258d77d70cad1b4f
budougumi0617 <budougumi0617@binc.jp>
2020-09-19 23:21:52 +0900

    :recycle: pick: get by id

--- cmd/main.go
+++ cmd/main.go
@@ -5,13 +5,14 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"strconv"
 
 	"github.com/budougumi/leetgode"
 )
 
 const (
-	LIST = "list"
-	PICK = "pick"
+	LIST     = "list"
+	PICK     = "pick"
 	GENERATE = "generate"
 )
 
@@ -34,10 +35,24 @@ func main() {
 		os.Exit(1)
 	}
 	switch sub {
+	// TODO: rustのCLIはこういうフォーマット
+	// ログインしていないと、解答済みかわからない。
+	//✔ [  1 ] Two Sum                                                      Easy   (45.52 %)
+	//[  2 ] Add Two Numbers                                              Medium (33.63 %)
+	//[  3 ] Longest Substring Without Repeating Characters               Medium (30.24 %)
+	//[  4 ] Median of Two Sorted Arrays                                  Hard   (29.34 %)
+	//[  5 ] Longest Palindromic Substring                                Medium (29.33 %)
+	//[  6 ] ZigZag Conversion                                            Medium (35.98 %)
+	//✔ [  7 ] Reverse Integer                                              Easy   (25.77 %)
 	case LIST:
 
 	case PICK:
-		q, err := cli.GetQuestion(ctx, args[1])
+		id, err := strconv.Atoi(args[1])
+		if err != nil || id == 0 {
+			fmt.Printf("cannot get id: %q\n", args[1])
+			os.Exit(1)
+		}
+		q, err := cli.GetQuestionByID(ctx, id)
 		if err != nil {
 			fmt.Printf("failed GetQuestion(%q): %v\n", args[1], err)
 			os.Exit(1)
