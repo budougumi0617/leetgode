7bb5678d374249a2ac79c4608ce68d7f82bf98c1
budougumi0617 <budougumi0617@binc.jp>
2020-09-19 23:20:22 +0900

    :sparkles: generate: save by id and slug

--- generate.go
+++ generate.go
@@ -68,9 +68,10 @@ func GenerateCmd(ctx context.Context, id int) error {
 		panic(err)
 	}
 	fmt.Printf("%s", buf.String())
-	// TODO: ファイル名を生成する
+	format := "tmp/%s.%s.go"
 	// TODO: どうやってファイル保存とテストしやすさを分けようか？
-	path := "tmp/hoge.go"
+	path := fmt.Sprintf(format, q.QuestionID, q.Slug)
+	fmt.Printf("save at %q\n", path)
 	if err := ioutil.WriteFile(path, buf.Bytes(), 0644); err != nil {
 		return err
 	}
