8093b4b02241b1aa4b2f9067fbc009f50aecb061
budougumi0617 <budougumi0617@binc.jp>
2020-09-28 22:49:39 +0900

    :sparkles: pick: add pick command

---
+++ pick.go
@@ -0,0 +1,39 @@
+package leetgode
+
+import (
+	"context"
+	"fmt"
+	"strconv"
+)
+
+var _ Cmd = &PickCmd{}
+
+type PickCmd struct {
+}
+
+func (c *PickCmd) Usage() string {
+	return "Pick a problem by id"
+}
+
+func (c *PickCmd) MaxArg() int {
+	return 1
+}
+
+func (c *PickCmd) Run(ctx context.Context, args []string) error {
+	cli, err := NewLeetCode()
+	if err != nil {
+		return err
+	}
+
+	id, err := strconv.Atoi(args[0])
+	if err != nil {
+		return err
+	}
+	q, err := cli.GetQuestionByID(ctx, id)
+	if err != nil {
+		return err
+	}
+	fmt.Printf("result: %#v\n", q)
+
+	return nil
+}
